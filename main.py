8a0fdbbf3cf2b779cb977f9d8ded603c0610833d
zeina101 <zeina.syedain@cas.edu.pk>
2025-04-24 21:00:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#program to count number of lines in this file
+# opening a file
+file = open("me.txt", "r")
+Counter = 0
+
+#reading from file
+Content = file.read()
+# splitting content into lines
+# and storing them in a list
+CoList = Content.split("\n")
+
+for i in range CFoList
+:
+if i:
+    Counter += 1
+
+    print("This is the number of lines in the file.")
+    print(Counter)
